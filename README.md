5509e3ea45e9a4ff86a004deac804df90d684175
Dylan Vorster <dylanvorster.dv@gmail.com>
2018-07-16 22:49:11 +0200

    build it on CI, and make base widget better

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# STORM React Core (WIP)
+# STORM React Core
+
+
+[![NPM](https://img.shields.io/npm/v/storm-react-forms.svg)](https://npmjs.org/package/@projectstorm/react-core)
+[![NPM](https://img.shields.io/npm/dt/storm-react-forms.svg)](https://npmjs.org/package/@projectstorm/react-core)
+[![CircleCI](https://circleci.com/gh/projectstorm/react-core/tree/master.svg?style=svg)](https://circleci.com/gh/projectstorm/react-core/tree/master)
+
+Base project for all the storm projects. It contains a bunch of helper widgets, tools and typescript
+objects for make glorius consistency.
 
--- src/widgets/BaseWidget.tsx
+++ src/widgets/BaseWidget.tsx
@@ -17,7 +17,7 @@ export interface BaseWidgetProps {
 	extraProps?: any;
 }
 
-export type BEMOptions = string | { [selector: string]: boolean };
+export type BEMOptions = string | string[] | { [selector: string]: boolean };
 
 export class BaseWidget<P extends BaseWidgetProps = BaseWidgetProps, S = any> extends React.Component<P, S> {
 	className: string;
@@ -32,15 +32,18 @@ export class BaseWidget<P extends BaseWidgetProps = BaseWidgetProps, S = any> ex
 	}
 
 	bem(selector: BEMOptions): string {
+		if(_.isArray(selector)){
+			return _.map(selector, (entry) => {
+				return this.buildClass(entry as string);
+			}).join(' ');
+		}
 		if (selector && typeof selector === "object") {
 			return _
-				.map(selector, (value: boolean, key: string) => {
-					if (!!value) {
-						return this.buildClass(key);
-					}
-					return "";
-				})
-				.join(" ");
+				.chain(selector)
+				.filter()
+				.map(this.buildClass.bind(this))
+				.value()
+				.join(' ');
 		}
 		return this.buildClass(selector as string);
 	}
@@ -50,7 +53,10 @@ export class BaseWidget<P extends BaseWidgetProps = BaseWidgetProps, S = any> ex
 		if (selector) {
 			className += " " + this.bem(selector);
 		}
-		return this.buildClass() + this.props.className || "";
+		if(this.props.className){
+			className += " " + this.props.className;
+		}
+		return className;
 	}
 
 	getProps(options?: BEMOptions): any {
